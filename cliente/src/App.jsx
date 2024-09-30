758dce4ff82f4998f0aad220c90244096e9b5298
Agustin Migliorero <agustinmigliorero@hotmail.com>
2024-09-30 13:30:42 -0300

    react router

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,12 +1,21 @@
 import Boton from "./componentes/Boton";
 import Tabla from "./componentes/Tabla";
+import Inicio from "./componentes/Inicio";
 import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>Hola gente!</h1>
-      <Tabla />
+      <h1 style={{ textAlign: "center", backgroundColor: "#ffaaaa" }}>
+        Este es el navbar de la pagina! <Link to="/">Inicio!</Link>{" "}
+        <Link to="/publicaciones">Publicaciones!</Link>
+      </h1>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/publicaciones" element={<Tabla />} />
+      </Routes>
     </div>
   );
 }
---
+++ cliente/src/componentes/Inicio.jsx
@@ -0,0 +1,11 @@
+function Inicio() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", color: "blue" }}>
+        Este es el inicio de la pagina!
+      </h1>
+    </div>
+  );
+}
+
+export default Inicio;
